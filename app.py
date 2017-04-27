01a12504c4053782c111ec485b1e85e1520487d2
Diego Alejandro Bañol <diegobanol@experimentality.co>
2017-04-27 02:14:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -28,15 +28,16 @@ def webhook():
     return r
 
 def makeWebhookResult(req):
-    if req.get("result").get("action") != "shipping.cost":
+    if req.get("result").get("action") != "asesorDeVentas": #Cambiar por action en el intent
         return {}
     result = req.get("result")
     parameters = result.get("parameters")
-    zone = parameters.get("shipping-zone")
+    zone = parameters.get("Productos")  #Cambiar por entity a recibir
+
 
     cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
 
-    speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
+    speech = "The cost of shipping to " + zone + " is " #+ str(cost[zone]) + " euros."
 
     print("Response:")
     print(speech)
