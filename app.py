0ff93ea72724069e0aa475c4b29570a727aeaaa1
Diego Alejandro Bañol <diegobanol@experimentality.co>
2017-04-27 02:46:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def makeWebhookResult(req):
 
     #cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
 
-    speech = "The cost of shipping to " + zone + " is " #+ str(cost[zone]) + " euros."
+    speech = "The cost of shipping to " + productos + " is " #+ str(cost[zone]) + " euros."
 
     print("Response:")
     print(speech)
